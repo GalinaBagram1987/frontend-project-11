5caa79055a1132410ed6c198d152b7c12df5dd41
GalinaBagram1987 <gaby_ev@mail.ru>
2025-04-29 11:06:50 +1000

    netlify.file

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,2 @@
-// Import our custom CSS
-// import '../scss/styles.scss';
-
-// Import all of Bootstrap's JS
-// import * as bootstrap from 'bootstrap';
-
 import '../scss/styles.scss';
 import 'bootstrap';
--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
       { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
       },
       {
         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
@@ -39,33 +39,6 @@ module.exports = {
       },
     ],
   },
-
-  // module: {
-  //     rules: [
-  // {
-  // test: /\.(scss)$/,
-  // use: [
-  // {
-  // loader: 'style-loader',
-  // },
-  // {
-  // loader: 'css-loader',
-  // },
-  // {
-  // loader: 'postcss-loader',
-  // options: {
-  // postcssOptions: {
-  // plugins: () => [require('autoprefixer')],
-  // },
-  // },
-  // },
-  // {
-  // loader: 'sass-loader',
-  // },
-  // ],
-  // },
-  // ],
-  // },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html',
@@ -75,49 +48,3 @@ module.exports = {
     clean: true,
   },
 };
-// import path from 'path'; // Используем import для импорта модуля path
-// import HtmlWebpackPlugin from 'html-webpack-plugin';
-
-// export default {
-//   mode: process.env.NODE_ENV || 'development',
-//   devServer: {
-//     client: {
-//       overlay: false,
-//     },
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/preset-env'],
-//           },
-//         },
-//       },
-//       { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
-// {
-//   test: /\.scss$/,
-//   use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-//       },
-//       {
-//         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-//         use: 'url-loader?limit=10000',
-//       },
-//       {
-//         test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-//         use: 'file-loader',
-//       },
-//     ],
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: 'index.html',
-//     }),
-//   ],
-//   output: {
-//     clean: true,
-//   },
-// };
