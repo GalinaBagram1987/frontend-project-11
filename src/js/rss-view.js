4b516d77a3fe53a32a3e5bed387ed6457b594062
GalinaBagram1987 <gaby_ev@mail.ru>
2025-05-28 15:05:28 +1000

    del conflict

--- src/js/rss-view.js
+++ src/js/rss-view.js
@@ -1,8 +1,4 @@
 import { watchedState } from './state.js';
-<<<<<<< HEAD
-// import i18next from './i18next.js';
-=======
->>>>>>> 253dfdbe736d02ec9f2579b8fd13168d5eed4abc
 
 const renderErrors = () => {
   const urlInput = document.querySelector('#url-input');
