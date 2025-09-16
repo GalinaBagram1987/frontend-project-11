6631126c244ad946580df7ee397e0baf03af2408
GalinaBagram1987 <gaby_ev@mail.ru>
2025-09-16 14:51:00 +1000

    change index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-import './src/scss/styles.scss';
-import 'bootstrap';
+// import './src/scss/styles.scss';
+// import 'bootstrap';
+
+if (process.env.NODE_ENV !== 'test') {
+  import('../scss/styles.scss');
+  import('bootstrap');
+}
+
 import rssLogic from './src/js/rss-model.js';
 
 rssLogic();
