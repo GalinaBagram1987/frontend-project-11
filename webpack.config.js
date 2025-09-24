5d0e54c7f86008590f2447a0b14fd80c0f683ccf
GalinaBagram1987 <gaby_ev@mail.ru>
2025-09-24 10:10:30 +1000

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,73 +2,7 @@ import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin'; // Импортируем HtmlWebpackPlugin
 import { fileURLToPath } from 'url';
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// const config = {
-//   mode: 'development', // или 'production
-//   entry: './index.js',
-//   devServer: {
-//     static: path.resolve(__dirname),
-//     port: 8081,
-//     hot: false, // Выключаем HMR
-//     client: {
-//       overlay: false, // не экранируем ошибки
-//     },
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['@babel/preset-env'],
-//           },
-//         },
-//       },
-//       { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
-//       {
-//         test: /\.scss$/,
-//         use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-//       },
-//       {
-//         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-//         use: 'url-loader?limit=10000',
-//       },
-//       {
-//         test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-//         use: 'file-loader',
-//       },
-//     ],
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: 'index.html',
-//     }),
-//   ],
-//   output: {
-//     clean: true,
-//   },
-//   optimization: {
-//     minimize: true,
-//     minimizer: [
-//       new TerserPlugin({
-//         terserOptions: {
-//           compress: {
-//             drop_console: false, // Убедитесь, что эта опция установлена
-//           },
-//         },
-//         extractComments: false,
-//       }),
-//     ],
-//   },
-// };
-// export default config;
-
 // настройки без bable
-// Без babel: вычисляем __dirname/filename через URL API
 // Без babel: вычисляем __dirname/filename через URL API
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -84,8 +18,9 @@ const config = {
         usePolling: false,
       },
     },
-    port: 8081,
-    hot: true, // Включаем HMR
+    port: 8080,
+    hot: false, // Включаем HMR
+    liveReload: false,
     client: {
       overlay: false, // не экранируем ошибки
     },
@@ -125,23 +60,11 @@ const config = {
   optimization: {
     minimize: false, // Отключаем в development
   },
-  // optimization: {
-  //   minimize: true,
-  //   minimizer: [
-  //     new TerserPlugin({
-  //       terserOptions: {
-  //         compress: {
-  //           drop_console: false, // Убедитесь, что эта опция установлена
-  //         },
-  //       },
-  //       extractComments: false,
-  //     }),
-  //   ],
-  // },
   watchOptions: {
     aggregateTimeout: 500, // Задержка перед пересборкой
     ignored: ['**/node_modules', '**/dist'], // Игнорируемые пути
   },
+  watch: false,
 };
 
 export default config;
