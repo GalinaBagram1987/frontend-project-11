8cf2a8fb011fcd1a5759fb0bc922a613e9f6b465
GalinaBagram1987 <gaby_ev@mail.ru>
2025-09-26 13:56:11 +1000

    change webp conf, ind.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import rssLogic from './js/rss-model.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  rssLogic();
-});
+rssLogic();
