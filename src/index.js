12d81edb3935193f3e81cb2a75deab14723c6a37
GalinaBagram1987 <gaby_ev@mail.ru>
2025-09-26 14:41:41 +1000

    correct semicol in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import rssLogic from './js/rss-model.js';
-
-rssLogic();
+// prettier-ignore
+import 'bootstrap'
+// prettier-ignore
+import 'bootstrap/dist/css/bootstrap.min.css'
+// prettier-ignore
+import rssLogic from './js/rss-model.js'
+// prettier-ignore
+rssLogic()
